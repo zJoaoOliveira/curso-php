338c8a599e5410875e05147047806ad13f89c9bb
DanielReis <idanielreiss@gmail.com>
2019-08-31 21:35:18 -0300

    tipos de dados e fix comentarios

---
+++ 3-PHP-Básico/3.3-Tipos-de-dados/exemplos/boolean1.php
@@ -0,0 +1,7 @@
+<?php
+var_dump(True);
+var_dump(False);
+var_dump(true);
+var_dump(false);
+var_dump(TrUe);
+var_dump(fAlSe);
---
+++ 3-PHP-Básico/3.3-Tipos-de-dados/exemplos/integer1.php
@@ -0,0 +1,11 @@
+<?php
+var_dump(1234); // número decimal
+var_dump(-123); // um número negativo
+var_dump(0123); // número octal (equivalente a 83 em decimal)
+var_dump(0x1A); // número hexadecimal (equivalente a 26 em decimal)
+var_dump(0b11111111); // número binário (equivalente ao 255 decimal)
+var_dump(1234); // número decimal
+var_dump(-123); // um número negativo
+var_dump(0123); // número octal (equivalente a 83 em decimal)
+var_dump(0x1A); // número hexadecimal (equivalente a 26 em decimal)
+var_dump(0b11111111); // número binário (equivalente ao 255 decimal)
